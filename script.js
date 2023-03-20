b6fac8bc6420dac65105356e059bed9cc1a5e93e
Felipe Silva Coelho <111004133+fs-coelho@users.noreply.github.com>
2023-03-20 14:41:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({resert:true});
+
+sr.reveal('.area-1', {duration: 1000});
+
+sr.reveal('.area-2', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2000
+});
+
+sr.reveal('.area-3', { scale: 0, duration:2000});
